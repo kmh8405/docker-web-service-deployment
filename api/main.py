8e48b1f6ddb93dd80ab07e6a2cf48839807c62e6
kmh8405 <kmh.ssem8405@gmail.com>
2026-04-17 14:18:57 +0900

    feat: Day 3 - add Redis queue (LPUSH) and Pub/Sub streaming for chat endpoint

--- api/main.py
+++ api/main.py
@@ -1,12 +1,43 @@
-from fastapi import FastAPI
-from sqlalchemy import text
-from connection import SessionFactory
+import uuid
+import json
+
+from fastapi import FastAPI, Body
+from fastapi.responses import StreamingResponse
+from redis import asyncio as aredis
+
+redis_client = aredis.from_url("redis://redis:6379", decode_responses=True) # redis://<ip>:<port>. 앞의 redis와 ip의 redis는 다름
 
 app = FastAPI()
 
-@app.get("/health-check")
-def health_check_handler():
-    with SessionFactory() as session:
-        stmt = text("SELECT * FROM user LIMIT 1;")
-        row = session.execute(stmt).fetchone() # Row 객체로 변환
-    return {"user": row._asdict()} # Row -> dict로 변환하기 위해 ._asdict() 사용
+@app.post("/chats")
+async def generate_chat_handler(
+    # 1) 요청 본문: user_input
+    user_input: str = Body(..., embed=True),
+):
+    # 2) 채널 구독
+    channel = str(uuid.uuid4())
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(channel)
+
+    # 3) Queue를 통해 Worker에 Task를 전달(enqueue)
+    task = {"channel": channel, "user_input": user_input}
+    await redis_client.lpush("queue", json.dumps(task))
+
+    # 4) 채널 메시지 읽고, 토큰 반환
+    async def event_generator():
+        async for message in pubsub.listen():
+            if message["type"] != "message":
+                continue
+
+            token = message["data"]
+            if token == "[DONE]":
+                break
+            yield token
+        await pubsub.unsubscribe(channel)
+        await pubsub.close() # 리소스 정리
+
+    # 5) 결과 수신
+    return StreamingResponse(
+        event_generator(),
+        media_type="text/event-stream",
+    )
